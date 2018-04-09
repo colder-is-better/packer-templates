3fb285dab7219ebada48240725cae999a2b724a1
Christos Varelas <subzraw@colder.xyz>
2018-04-09 22:47:57 +0200

    Upload first batch of files

---
+++ provisioners/leap423srv.prov.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+REPO_OSS="http://ftp.uni-erlangen.de/opensuse/distribution/leap/42.3/repo/oss/"
+REPO_NON_OSS="http://ftp.uni-erlangen.de/opensuse/distribution/leap/42.3/repo/non-oss/"
+
+REPO_OSS_UPDATES="http://ftp.uni-erlangen.de/opensuse/update/leap/42.3/oss/"
+REPO_NON_OSS_UPDATES="http://ftp.uni-erlangen.de/opensuse/update/leap/42.3/non-oss/"
+
+printf "[prov_script] removing installation media repositories...\n"
+
+/usr/bin/zypper -q rr 1
+
+printf "[prov_script] adding online repositories...\n"
+
+/usr/bin/zypper -q ar -n leap423-oss "$REPO_OSS" oss
+/usr/bin/zypper -q ar -n leap423-non-oss "$REPO_NON_OSS" non-oss
+/usr/bin/zypper -q ar -f -n leap423-oss-up "$REPO_OSS_UPDATES" oss-up
+/usr/bin/zypper -q ar -f -n leap423-non-oss-up "$REPO_NON_OSS_UPDATES" non-oss-up
+
+printf "[prov_script] refreshing newly added repositories...\n"
+
+/usr/bin/zypper -q ref
+
+printf "[prov_script] patching things up :P\n"
+
+/usr/bin/zypper -n patch > /dev/null 2>&1
+zypper_ec=$?
+
+if [ $zypper_ec -eq 103 ]; then
+	printf "[prov_script] zypper has been updated, re-running it now...\n"
+	/usr/bin/zypper -n patch > /dev/null 2>&1
+elif [ $zypper_ec -ne 0 ]; then
+	printf "[prov_script] *** something went wrong, zypper returned error code %d ***\n", $zypper_ec
+    exit 1
+fi
+
+printf "[prov_script] updating any packages that cannot be patched...\n"
+
+/usr/bin/zypper -n up > /dev/null 2>&1
+
+printf "[prov_script] all done, enjoy your openSUSE!\n"
+
+exit 0
